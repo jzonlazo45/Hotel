c8b0f3742bad63c27a5ba454929e71d7111ba034
Jason Lazo <jasonlazo.ph1@gmail.com>
2020-08-23 16:58:59 +0800

    updated 8-23

--- js/common.js
+++ js/common.js
@@ -1,28 +1,44 @@
+// Generate Navbar + Footer Start
 
 $(document).ready(function(){
     $("#navbar").load("fragments/navbar/navbar-logo.html"); 
     $("#footer").load("fragments/footer/footer-basic.html");       
 });
 
-var isNightMode=false;
+// Generate Navbar + Footer End
+
+// Night Mode Start
+
+if (localStorage.getItem("isNightMode") === null) {
+    localStorage.setItem("isNightMode", false);
+}else if(localStorage.getItem("isNightMode") === "false"){
+    $(':root').css('--main-primary-color', '#dd7631');
+    $(':root').css('--main-primary-color-hover', '#708160');
+    $(':root').css('--body-bg', 'white');
+    $(':root').css('--body-text-color', 'black');
+}else{
+    $(':root').css('--main-primary-color', '#1f1f1f');
+    $(':root').css('--main-primary-color-hover', '#32DBA3');
+    $(':root').css('--body-bg', '#121212');
+    $(':root').css('--body-text-color', 'white');
+}
 
 function nightmodeToggle(){
-
-    console.log('click works');
-
-    if (isNightMode){
+    if (localStorage.getItem("isNightMode") === "true"){
         $(':root').css('--main-primary-color', '#dd7631');
         $(':root').css('--main-primary-color-hover', '#708160');
         $(':root').css('--body-bg', 'white');
         $(':root').css('--body-text-color', 'black');
-        isNightMode = false;
+        localStorage.setItem("isNightMode", false);
     }
     else{
         $(':root').css('--main-primary-color', '#1f1f1f');
         $(':root').css('--main-primary-color-hover', '#32DBA3');
         $(':root').css('--body-bg', '#121212');
         $(':root').css('--body-text-color', 'white');
-        isNightMode = true;        
+        localStorage.setItem("isNightMode", true);
     }
 
 }
+
+// Night Mode End
